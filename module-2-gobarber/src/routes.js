17cd757ce34b7d30948566a20850bc7896747f07
Luan Carlos <luancarlos@pop-os.localdomain>
2019-08-01 22:52:20 -0300

    added the db config and installed the sequelize and eslint

--- module-2-gobarber/src/routes.js
+++ module-2-gobarber/src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from 'express'
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'Hello world'});
-});
+routes.get('/', (req, res) => res.json({ hello: 'Hello world' }));
 
 export default routes;
