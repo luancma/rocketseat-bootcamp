7ca08ffbee30aa00df1d8f80b4f00ce64512f6e5
Luan Carlos <luancarlos@pop-os.localdomain>
2019-08-13 21:50:58 -0300

    creating the avatar_id

--- module-3-gobarber/src/database/index.js
+++ module-3-gobarber/src/database/index.js
@@ -1,9 +1,6 @@
 import Sequelize from 'sequelize';
-
 import User from '../app/models/Users';
-
 import File from '../app/models/File';
-
 import databaseConfig from '../config/database';
 
 const models = [User, File];
@@ -16,7 +13,9 @@ class Database {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
