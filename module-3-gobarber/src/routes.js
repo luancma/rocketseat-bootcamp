27ce6992bb849c242afb13c804b8ab4f4fb1ebb0
dev <dev@localhost.localdomain>
2019-09-05 23:23:42 -0300

    Created the notification of appointments, the config of mongodb, update notification and canceletaion of appointments

--- module-3-gobarber/src/routes.js
+++ module-3-gobarber/src/routes.js
@@ -8,6 +8,8 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -24,7 +26,12 @@ routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/appointments', AppointmentController.index);
-
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 export default routes;
