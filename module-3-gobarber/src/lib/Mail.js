910555d82449a89bc00fa144b070aa8f1cbe4df7
Luan <luancma18@gmail.com>
2019-09-08 14:49:37 -0300

    created the template email files config

--- module-3-gobarber/src/lib/Mail.js
+++ module-3-gobarber/src/lib/Mail.js
@@ -1,19 +1,42 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import exphbs from 'express-handlebars';
 import mailConfig from '../config/mail';
 
 class Mail {
   constructor() {
     const { host, port, secure, auth } = mailConfig;
-    this.transport = nodemailer.createTransport({
+    this.transporter = nodemailer.createTransport({
       host,
       port,
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTempletes();
+  }
+
+  configureTempletes() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
   }
 
   sendMail(message) {
-    return this.transport.sendMail({
+    return this.transporter.sendMail({
       ...mailConfig.default,
       ...message,
     });
